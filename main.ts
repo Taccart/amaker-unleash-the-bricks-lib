3c83a5a6e172fb20e43df00d9bc049d10a7df6d1
Thierry <thierry@accart.name>
2025-09-09 08:29:51 +0200

    main

---
+++ main.ts
@@ -0,0 +1,9 @@
+//% color=#4f0230 weight=1000 icon="\u26D0"
+namespace UnleashTheBricks2025 {
+    //% block="get Msg"
+    //% blockId=get_msg
+    //% advanced=true
+    //% description="Get a message"
+    export function getMsg() :string { return "Msg";}
+
+}
