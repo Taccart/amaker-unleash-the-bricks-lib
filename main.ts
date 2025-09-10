ef2acf92a18e10062b9b00985b345d162f264b50
Thierry <thierry@accart.name>
2025-09-10 07:59:43 +0200

    debuging the failure in lib loading

--- main.ts
+++ main.ts
@@ -1,20 +1,7 @@
-UTBBot.onMessageStopReceived(function () {
-    basic.showIcon(IconNames.No)
-})
-UTBBot.onMessageDangerReceived(function () {
-    basic.showIcon(IconNames.Skull)
-})
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
-    UTBBot.initAsBot(UTBBotCode.TeamName.RequiemForABot)
-    UTBBot.emitHeartBeat()
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.House)
-    UTBController.initAsController()
-    UTBController.sendActionStart()
-})
-UTBBot.onMessageStartReceived(function () {
-    basic.showIcon(IconNames.Yes)
-})
-basic.showIcon(IconNames.House)
+//% color=#300000 weight=1000 icon="\u25a3" 
+namespace UTB2025 {
+    //% blockId=utb2025_get_msg block="Get message"
+    //% block="Get message"
+    //% advanced=false
+    export function getMsg() :string { return "Msg";}
+}
