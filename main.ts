bce2670afd84fbd7415e6e0c82d313508fdea7a4
Thierry <thierry@accart.name>
2025-09-10 07:52:15 +0200

    Add a main

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
+import { UTBRadioCode } from "./UTBRadioCode.ts";
+
+import { UTBBotCode } from "./UTBBotCode.ts";    
+import { UTBControllerCode} from "./UTBControllerCode.ts";    
+import { UTBRadio} from "./UTBRadio.ts";
+import { UTBBot } from "./UTBBot.ts";    
+import {UTBController } from "./UTBController.ts";    
+
+
 //% color=#4f0230 weight=1000 icon="\u26D0"
 namespace UnleashTheBricks2025 {
+
+
     //% block="get Msg"
     //% blockId=get_msg
     //% advanced=true
